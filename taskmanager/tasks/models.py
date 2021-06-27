cfd708c679d8d2e88c96385f1e4e7a650de4e8f8
MKING301 <33675411+MKing301@users.noreply.github.com>
2021-06-27 17:04:06 -0400

    Change model from tasks to task

--- taskmanager/tasks/models.py
+++ taskmanager/tasks/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Tasks(models.Model):
+class Task(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=25, blank=True)
     body = models.CharField(max_length=250)
