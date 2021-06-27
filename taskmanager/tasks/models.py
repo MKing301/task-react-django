f20b7710e1c3ff0b42db63fbcfe34a0be3ad3ec7
MKING301 <33675411+MKing301@users.noreply.github.com>
2021-06-27 11:54:35 -0400

    Add tasks model to tasks app

--- taskmanager/tasks/models.py
+++ taskmanager/tasks/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Tasks(models.Model):
+    created_date = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=25, blank=True)
+    body = models.CharField(max_length=250)
+    completed = models.BooleanField(default=False)
