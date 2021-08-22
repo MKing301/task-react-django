e0fd3bd63762d0165e9013c660d291664779fc0b
MKING301 <33675411+MKing301@users.noreply.github.com>
2021-08-22 14:36:02 -0400

    Add default auth class to settings

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -127,5 +127,6 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSIONS_CLASSES': ('rest_framework.permissions.IsAuthenticatedOrReadOnly',)
+    'DEFAULT_PERMISSIONS_CLASSES': ('rest_framework.permissions.IsAuthenticatedOrReadOnly',),
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',)
 }
